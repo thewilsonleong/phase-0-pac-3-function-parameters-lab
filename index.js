809c1213420c68e76b52d4c67a62fde601c8d043
curlym <curlym@curlym.local>
2022-07-21 10:19:33 +1000

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+// assignment lab
+// function with a parameter
+function introduction(name) {
+  console.log(`Hi, my name is ${name}!`)
+}
+introduction("Ted")
+
+
+//assignment lab
+// function with two parameters
+function introductionWithLanguage(name, language) {
+  console.log(`Hi, my name is ${name} and I am learning to program in ${language}`)
+}
+introductionWithLanguage("Teddy", "Cantonese")
+
+//assignment lab
+// function with optional parameters
+function introductionWithLanguageOptional(name="User") {
+  console.log(`Hello, ${name}`)
+}
+introductionWithLanguageOptional("Wilson")
+introductionWithLanguageOptional()
+
+
